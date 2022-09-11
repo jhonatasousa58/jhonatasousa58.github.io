4b3c3eb27f679d107da71aedf6317e713106e920
Adelson Jhonata <jhonatasousa58@gmail.com>
2022-09-11 14:16:09 -0300

    testando

---
+++ script.js
@@ -0,0 +1,22 @@
+let botaoNao = document.querySelector('#botaoNao');
+let botaoSim = document.querySelector('#botaoSim');
+let contadorDeRecusadas = 0;
+botaoNao.onmouseover = esquivaDoBotao;
+botaoNao.onclick = RECUSADOOOOO;
+botaoSim.onclick = ELAACEITOUUUUU;
+
+function ELAACEITOUUUUU(){
+    let msgSim = "AAAAEEEEEEOOOOOO TE AMO DEMAIS MANITAAAAAAA, TAMO NAMORANDOOOOOOOO ❤️";
+    alert(msgSim);
+}
+
+function RECUSADOOOOO(){
+    let msgNao = "PQP em, depois de " + contadorDeRecusadas + " tentativas aparentemente você não quer namorar comigo nesse momentooooo. MMMMMAAAAAASSSSSSS NOIS É BR POHAAAA DESISTO NUNNNNCAAAAA";
+    alert(msgNao);
+}
+
+function esquivaDoBotao(){
+    botaoNao.style.top = Math.floor(Math.random() * 90) + '%';
+    botaoNao.style.left = Math.floor(Math.random() * 65) + '%';
+    contadorDeRecusadas++;
+}
